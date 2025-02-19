4a2ebbca41a055c0ed1011043c1e48753af64b5f
mkdirprince <mkdirprince@github.com>
2025-02-19 02:50:46 +0000

    define types

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Setting {
+	label: string;
+	type: string;
+	required: string;
+	default: string;
+}
+
+export interface MonitorPayload {
+	channel_id: string;
+	return_url: string;
+	settings: Setting[];
+}
