7fde2b4a91597ecb87860e1c595dadcbc5f4491c
mkdirprince <mkdirprince@github.com>
2025-02-19 19:25:36 +0000

    fix typo error with https

--- src/utils/validHttpsUrl.ts
+++ src/utils/validHttpsUrl.ts
@@ -2,7 +2,7 @@ export const validHttpsUrl = (site: string): URL | null => {
   try {
     const url = new URL(site);
 
-    return url.protocol === "htpps:" ? url : null;
+    return url.protocol === "https:" ? url : null;
   } catch {
     return null;
   }
