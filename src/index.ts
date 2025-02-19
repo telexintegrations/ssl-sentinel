55ae80a26c0bff5ee462667563778207559af0b7
mkdirprince <mkdirprince@github.com>
2025-02-19 20:58:44 +0000

    correct not adding node to the script and configure index.ts to work with .env file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import integrationRouter from "./routes/integration";
 import tickRouter from "./routes/tick";
 import middleware from "./utils/middleware";
+import "dotenv/config";
 
 export const app = express();
 app.use(cors());
@@ -13,7 +14,7 @@ app.use("/", tickRouter);
 
 app.use(middleware.unknownEndpoint);
 
-const PORT = 3000 | 3001;
+const PORT = process.env.PORT || 3000;
 
 export const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
