096c64e849dfa20b06d6cfb5f90848e231884793
mkdirprince <mkdirprince@github.com>
2025-02-19 17:35:17 +0000

    format types and remove index.ts

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 export interface Setting {
-	label: string;
-	type: string;
-	required: string;
-	default: string;
+  label: string;
+  type: string;
+  required: string;
+  default: string;
 }
 
 export interface MonitorPayload {
-	channel_id: string;
-	return_url: string;
-	settings: Setting[];
+  channel_id: string;
+  return_url: string;
+  settings: Setting[];
 }
